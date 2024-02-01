63515b13261e023fa9122071bdedea52ec90b1f8
Cine Student <Cine Student@CINELAB14>
2024-01-31 20:49:04 -0800

    Add project files.

---
+++ TheHangmanGame/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheHangmanGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Hangman Game");
+            Console.WriteLine("Guess the right word before the man gets hanged");
+
+            // this is the array thatholds the player names.
+            string[] guess= new string[] {"Mario","Luigi","Link","Kirby", "Samus" };
+
+        }
+    }
+}
